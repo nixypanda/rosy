37b27649e59f9f58a949040bb33a323261c9087d
Sherub Thakur <sherub.thakur@gmail.com>
2022-05-16 16:40:22 +0530

    Print hello world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
-fn main() {
-    println!("Hello")
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
 
