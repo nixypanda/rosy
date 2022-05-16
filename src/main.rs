8b0c8365fa2a4b48426256d40d2280d98bf0e631
Sherub Thakur <sherub.thakur@gmail.com>
2022-05-17 03:41:02 +0530

    Basic panic handler

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World!");
+    panic!("Something wrong happened!");
 
     loop {}
 }
