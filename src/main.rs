cfa7287432423ab1ccdb97ba0dd909da624b6300
Sherub Thakur <sherub.thakur@gmail.com>
2022-05-19 22:39:53 +0530

    Add basic testing setup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -16,5 +19,23 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello World!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
